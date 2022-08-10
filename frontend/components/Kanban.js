2b7ce6881e206f82ff55333a9c6bd2047f91efbd
Thomas Sleeman <sleemantom@gmail.com>
2022-08-10 16:09:01 +0100

    Small commit

--- frontend/components/Kanban.js
+++ frontend/components/Kanban.js
@@ -1,9 +1,9 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Column from './Column';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import initialData from './initial-data';
-import { useGlobalContext } from '../context';
+import { useGlobalContext } from '../utils/context';
 import styled from 'styled-components';
 
 const Container = styled.section`
@@ -11,107 +11,37 @@ const Container = styled.section`
 `;
 
 const Kanban = () => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const { jobsData } = useGlobalContext();
-  console.log(jobsData);
-  const [data, setData] = useState(initialData);
+  const { jobsData, catsData } = useGlobalContext();
+  console.log('jobsData:', jobsData);
+  // console.log(jobsData.data);
 
-  const onDragEnd = (result) => {
-    const { destination, source, draggableId, type } = result;
-
-    if (!destination) {
-      return;
-    }
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
-
-    if (type === 'column') {
-      const newColumnOrder = Array.from(data.columnOrder);
-      newColumnOrder.splice(source.index, 1);
-      newColumnOrder.splice(destination.index, 0, draggableId);
-
-      const newData = {
-        ...data,
-        columnOrder: newColumnOrder,
-      };
-      setData(newData);
-      return;
-    }
-
-    const start = data.columns[source.droppableId];
-    const finish = data.columns[destination.droppableId];
-
-    // Moving within the same list
-    if (start === finish) {
-      const newTaskIds = Array.from(start.taskIds);
-      newTaskIds.splice(source.index, 1);
-      newTaskIds.splice(destination.index, 0, draggableId);
-      const newColumn = {
-        ...start,
-        taskIds: newTaskIds,
-      };
-      const newData = {
-        ...data,
-        columns: {
-          ...data.columns,
-          [newColumn.id]: newColumn,
-        },
-      };
-      setData(newData);
-    }
-
-    //Moving from one list to another
-    const startTaskIds = Array.from(start.taskIds);
-    startTaskIds.splice(source.index, 1);
-    const newStart = {
-      ...start,
-      taskIds: startTaskIds,
-    };
-    const finishTaskIds = Array.from(finish.taskIds);
-    finishTaskIds.splice(destination.index, 0, draggableId);
-    const newFinish = {
-      ...finish,
-      taskIds: finishTaskIds,
-    };
-
-    const newData = {
-      ...data,
-      columns: {
-        ...data.columns,
-        [newStart.id]: newStart,
-        [newFinish.id]: newFinish,
-      },
-    };
-    setData(newData);
-  };
+  console.log('catsData:', catsData);
+  // console.log(catsData.data);
+  // const [data, setData] = useState(initialData);
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="all-columns" direction="horizontal" type="column">
-        {(provided) => (
-          <Container {...provided.droppableProps} ref={provided.innerRef}>
-            {data.columnOrder.map((columnId, index) => {
-              const column = data.columns[columnId];
-              const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
-              return (
-                <Column
-                  key={column.id}
-                  column={column}
-                  tasks={tasks}
-                  index={index}
-                />
-              );
-            })}
-            {provided.placeholder}
-          </Container>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <Droppable droppableId="all-columns" direction="horizontal" type="column">
+      {(provided) => (
+        <Container {...provided.droppableProps} ref={provided.innerRef}>
+          {data.columnOrder.map((columnId, index) => {
+            const column = data.columns[columnId];
+            const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+            return (
+              <Column
+                key={column.id}
+                column={column}
+                tasks={tasks}
+                index={index}
+              />
+            );
+          })}
+          {provided.placeholder}
+        </Container>
+      )}
+    </Droppable>
+    // </DragDropContext>
   );
 };
 
